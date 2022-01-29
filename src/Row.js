a640990eb17074122129b6ba0eb03dc66db2e9ce
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-29 15:49:52 +0600

    movie posters link added

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from "react";
 import axios from './axios';
+import './Row.css'
+
+const base_url ="https://image.tmdb.org/t/p/original"
 
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
@@ -23,7 +26,7 @@ function Row({title, fetchUrl}) {
                 {movies.map(movie => (
                     <img
                         className="row__poster"
-                        src={`${movie.poster_path}`} alt={movie.name}/>
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
                 ))}
             </div>
         </div>
