c31fdb7c64d99d0310a7aec2a565403334116e6f
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-29 15:28:12 +0600

    movie posters added

--- src/Row.js
+++ src/Row.js
@@ -1,23 +1,31 @@
 import React, {useEffect, useState} from "react";
-import axios from "axios";
+import axios from './axios';
 
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
 
-    useEffect(
-        () => {
+    useEffect(() => {
             async function fetchData() {
-            const request = await axios.get(fetchUrl);
-                console.log(request);
-                return request
+                const request = await axios.get(fetchUrl);
+                setMovies(request.data.results)
+                return request;
             }
-            fetchData()
-        }, []
+            fetchData();
+        }, [fetchUrl]
     );
 
+    console.log(movies)
+
     return (
-        <div>
+        <div className='row'>
             <h2>{title}</h2>
+            <div className='row__posters'>
+                {movies.map(movie => (
+                    <img
+                        className="row__poster"
+                        src={`${movie.poster_path}`} alt={movie.name}/>
+                ))}
+            </div>
         </div>
     )
 }
--- src/requests.js
+++ src/requests.js
@@ -1,10 +1,12 @@
-const API_KEY = 'e388c066698fed551501fb44d5922327';
+
+
+const apiKey = 'e388c066698fed551501fb44d5922327';
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginal: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`
+    fetchTrending: `/trending/all/week?api_key=${apiKey}&language=en-US`,
+    fetchNetflixOriginal: `/discover/tv?api_key=${apiKey}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${apiKey}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`
 }
 
 export default requests
