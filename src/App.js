49d881ea2524431ee7ffb4acfff3686544d1e195
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-29 14:14:54 +0600

    fetch data props added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     return (
         <div className="App">
             <h1>I am making Netflix Clone</h1>
-            <Row title="NETFLIX ORIGINALS" fetchUrl={fetchNetflixOriginal}/>
-            <Row title="Trending Now" fetchUrl={fetchTrending}/>
+            <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginal}/>
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
         </div>
     );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
-function Row({title}) {
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(
+        () => {
+            async function fetchData() {
+            const request = await axios.get(fetchUrl);
+                console.log(request);
+                return request
+            }
+            fetchData()
+        }, []
+    );
+
     return (
         <div>
             <h2>{title}</h2>
