e5d539660d00dcb87ca9318e21c0130c76dc7e85
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-29 13:14:06 +0600

    Row js added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from "react";
+import Row from "./Row";
 
 function App() {
     return (
         <div className="App">
             <h1>I am making Netflix Clone</h1>
         </div>
+
+        <Row title="NETFLIX ORIGINALS"/>
+        <Row title="Trending Now"/>
     );
 }
 
--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,8 @@
 const API_KEY = 'e388c066698fed551501fb44d5922327';
 
 const requests = {
-
-    API_KEY
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginal: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`
 }
